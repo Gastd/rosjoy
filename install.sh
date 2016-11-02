6ffb311ac8e7d093b9d39bf95553ae8cd5945bbd
Gabriel Araujo <gabriel.araujo.5000@gmail.com>
2016-11-02 18:36:10 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ sudo systemctl start docker
 sudo docker run hello-world
 sudo systemctl enable docker
 sudo /usr/sbin/usermod -a -G docker aluno
-mkdir docker && cd docker
+mkdir docker
+cd /root/docker
 wget https://raw.githubusercontent.com/Gastd/rosjoy/master/Dockerfile
 sudo time docker build -t rosjoy .
